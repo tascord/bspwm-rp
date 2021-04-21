4a09155f053b57c9cc1c4306de011909440ef95d
cayke <thecactivecake@gmail.com>
2021-04-21 16:04:26 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-const client = require('discord-rich-presence')('-- your client id here --');
-const monitor = require('active-window');
-const startTime = Date.now();
+const client = require('discord-rich-presence')('-- your client id here --'),
+      monitor = require('active-window'),
+      startTime = Date.now();
 
 setInterval(() => {
   
-  try { monitor.getActiveWindow(update); } 
-  catch (err) { console.log(err); }
-
+  try {
+    monitor.getActiveWindow(update);
+  } catch (error) {
+    console.log(error);
+  }
 
 }, 5000);
 
 console.log('[+] Started RP Server');
 
-function update(window) {
+function update (window) {
 
-  var app = window.app.split('", "')[0].slice(1);
-
-  var user = process.env['USER'];
-  var window = window.title;
+  const app = window.app.split('", "')[0].slice(1)
+        user = process.env['USER'],
+        window = window.title;
 
   client.updatePresence({
-    state: `${window}`,
+    state: window.toString(),
     details: `${user}@${app}`,
     startTimestamp: startTime,
     largeImageKey: 'bspwm',
     smallImageKey: 'somethingelse',
-    instance: true,
+    instance: true
   });
 
 }
